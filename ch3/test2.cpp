fa41e276a9f91ae240df9b680ca663e0091a2e35
xinbai <xbai@asiatelco.com>
2019-07-01 18:50:55 -0700

    add string class

---
+++ ch3/test2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    string line;
+    while (getline(cin, line)) {
+        cout << line << endl;
+    }
+    return 0;
+}
+
